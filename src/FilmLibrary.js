927b909eabca13831ed1eda457b8a6e4df992f35
K.M WONG <30794641+kam89@users.noreply.github.com>
2022-11-22 17:31:21 +0800

    ✨ Add api call to TMDB

--- src/FilmLibrary.js
+++ src/FilmLibrary.js
@@ -1,27 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FilmDetail, { FilmDetailEmpty } from "./FilmDetail";
 import './FilmLibrary.css';
 import FilmRow from "./FilmRow";
-import TMDB from "./TMDB";
+import { TMDB, TMDB_API_KEY } from "./TMDB";
 
 
 function FilmLibrary() {
 
   // const films = TMDB.films;
-  const [films, setFilms] = useState(TMDB.films);
-  const [currentList, setCurrentList] = useState(films);
+  const [films, setFilms] = useState([]);
   const [selectedFilm, setSelectedFilm] = useState(null);
   const [favourite, setFavourite] = useState([]);
   const [isFavouriteView, setIsFavouriteView] = useState(false);
 
   const handleonSelect = (film) => {
-    setSelectedFilm(film);
+    const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB_API_KEY}&language=en-US`;
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        setSelectedFilm(data);
+      });
+    });
+  };
+
+  const loadMore = () => {
+    const url = `https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate&primary_release_year=2022`;
+    fetch(url).then((response) => {
+      response.json().then((data) => {
+        setFilms(data.results);
+      });
+    });
   };
 
   const handleFavourite = (film, action) => {
     let newFavouriteList = favourite.slice();
-    const favIndex = favourite.findIndex(item => item.id === film.id);
-    const index = films.findIndex(item => item.id === film.id);
+    const favIndex = newFavouriteList.findIndex(item => item.id === film.id);
+
     if (action === "add") {
       newFavouriteList = [film, ...newFavouriteList];
       film.isFavourite = true;
@@ -34,15 +47,12 @@ function FilmLibrary() {
     setFavourite(newFavouriteList);
 
     const newFilmsList = films.slice();
+    const index = newFilmsList.findIndex(item => item.id === film.id);
     newFilmsList.splice(index, 1, film);
     setFilms(newFilmsList);
-
-    if (isFavouriteView) return setCurrentList(newFavouriteList);
-    setCurrentList(newFilmsList);
   };
 
   const handleChangeView = (value) => {
-    setCurrentList(value ? favourite : films);
     setIsFavouriteView(value);
   };
 
@@ -60,12 +70,15 @@ function FilmLibrary() {
             <span className="section-count">{favourite.length}</span>
           </button>
         </div>
-        {currentList.map((item, index) => <FilmRow
+        {(isFavouriteView ? favourite : films).map((item, index) => <FilmRow
           key={index}
           onReadMore={() => handleonSelect(item)}
           onHandleFavourite={(action) => handleFavourite(item, action)}
           {...item}
         />)}
+        <div className="film-details">
+          <button className="film-details" onClick={loadMore}>Load More</button>
+        </div>
       </div>
       <div className="film-details">
         <h1 className="section-title">DETAILS</h1>
@@ -74,7 +87,7 @@ function FilmLibrary() {
           :
           <FilmDetail {...selectedFilm} />}
       </div>
-    </div>
+    </div >
   );
 }
 
